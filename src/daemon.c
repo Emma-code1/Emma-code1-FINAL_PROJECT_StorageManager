88c2dcc348ee995d441a58b132f31cd2a6f7bb4c
Emmanuel Castillo <emmanuelcastilloleyva@gmail.com>
2025-11-30 19:15:03 -0600

    Delete src directory

--- src/daemon.c
+++
@@ -1,3 +0,0 @@
-#include <stdio.h>
-int daemon_init(void){ printf("daemon init stub\n"); return 0; }
-int daemon_shutdown(void){ printf("daemon shutdown stub\n"); return 0; }
--- src/ipc_server.c
+++
@@ -1,5 +0,0 @@
-#include "ipc_server.h"
-#include <stdio.h>
-int ipc_server_init(const char *socket_path){ printf("ipc init %s\n", socket_path); return 0;}
-int ipc_server_run(void){ printf("ipc run (stub)\n"); return 0;}
-int ipc_handle_request(int client_fd, request_t *req, response_t *resp){ return 0;}
